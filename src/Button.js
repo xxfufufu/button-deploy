ee85cc089cd545bc6208df7d8ec3f0bcdaa4e006
xxfufufu <xxfufufu24@gmail.com>
2021-03-07 20:11:59 +0700

    benerin klik dan media

--- src/Button.js
+++ src/Button.js
@@ -4,7 +4,7 @@ function Button (props)
     const styling = `${props.variant} ${props.size} ${ninja} ${props.color} && ${props.endIcon || props.startIcon ? 'icon' : ''}`;
     let names = props.endIcon !== '' ? 'endIcon' : props.startIcon !== '' ? 'startIcon' : props.color === '' || props.color === 'default' ? 'Default':  props.color === 'primary'? 'Primary' : props.color === 'secondary' ? 'Secondary' :'Danger';
     return (
-        <button onClick={() => alert('ki,love you~')} disabled={props.disabled} className={styling}><span className='material-icons'>{props.startIcon}</span><span>{names}</span><span className='material-icons'>{props.endIcon}</span></button>
+        <button onClick={() => alert('ki, love you~')} disabled={props.disabled} className={styling}><span className='material-icons'>{props.startIcon}</span><span>{names}</span><span className='material-icons'>{props.endIcon}</span></button>
     )
 }
 
